a686bc54921a8e0c478b4673bea37721135d351d
simoxxhunter <kingstonegame@gmail.com>
2024-06-13 16:46:29 +0100

    interface for serie

---
+++ src/main/java/com/example/MovieBackend/dao/SerieInterface.java
@@ -0,0 +1,9 @@
+package com.example.MovieBackend.dao;
+
+import com.example.MovieBackend.model.FilmModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface SerieInterface  extends JpaRepository<FilmModel,Integer> {
+}
